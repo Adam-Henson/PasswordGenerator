23b711e49198a194421761ddb5dde3065699d38d
Adam Henson <hensonadam4@yahoo.com>
2022-09-23 21:04:56 -0500

    Add project files.

---
+++ PasswordGenerator/Controllers/passwordController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordGenerator.Models;
+using System.Text;
+
+namespace PasswordGenerator.Controllers
+{
+    public class passwordController : Controller
+    {
+        public void Password()
+        {
+            //static void Main(string[] args)
+            //{
+            //    Console.Write("Enter the length:");
+            //    int length = Convert.ToInt32(Console.ReadLine());
+
+            //    if (length > 0)
+            //    {
+            //        Console.WriteLine(GeneratePassword(length));
+            //    }
+            //}
+            ////public static string GeneratePassword(int length)
+            //{
+            //    const string chars = "1234567890" + "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "!@#$%^&*_+=-";
+
+            //    StringBuilder stringBuilder = new StringBuilder();
+            //    Random random = new Random();
+
+            //    for (int i = 0; i < length; i++)
+            //    {
+            //        int index = random.Next(chars.Length);
+            //        stringBuilder.Append(chars[index]);
+            //    }
+
+            //    return stringBuilder.ToString();
+            //}
+        }
+        //public string GetPassword()
+        //{
+
+        //}
+        public IActionResult Index()
+        {
+            passwordModel model = new passwordModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string password, int numbers, string upper, string lower, bool hasPassword)
+        {
+            passwordModel model = new passwordModel(password, numbers, upper, lower, hasPassword);
+
+            return View(model);
+        }
+    }
+}
---
+++ PasswordGenerator/Models/passwordModel.cs
@@ -0,0 +1,23 @@
+﻿namespace PasswordGenerator.Models
+{
+    public class passwordModel
+    {
+        public string Password { get; set; }
+        public int Numbers { get; set; }
+        public string Upper { get; set; }
+        public string Lower { get; set; }
+        public bool HasPassword { get; set; }
+        public passwordModel(string password, int numbers, string upper, string lower, bool hasPassword)
+        {
+            Password = password;
+            Numbers = numbers;
+            Upper = upper;
+            Lower = lower;
+            HasPassword = hasPassword;
+        }
+        public passwordModel()
+        {
+
+        }
+    }
+}
